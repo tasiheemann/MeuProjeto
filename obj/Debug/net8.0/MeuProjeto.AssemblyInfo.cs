c8e7435eaa7b4e81ec05461fab33225efe13f983
Tasiana Heemann <tasiheemann16@gmail.com>
2025-08-06 12:48:44 -0300

    primeiro-teste

--- obj/Debug/net8.0/MeuProjeto.AssemblyInfo.cs
+++ obj/Debug/net8.0/MeuProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45d70282a04e073d60aa88dac0f93e78f456b8db")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
